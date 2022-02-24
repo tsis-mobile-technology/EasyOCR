83875bcfda9d0619d4ad761bc1dd40b5997b55ea
gotaejong <gainworld@gmail.com>
2022-02-24 16:39:19 +0900

    Test Code

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import cv2 as cv
 from easyocr.easyocr import *
 
 # GPU 설정
@@ -16,6 +17,21 @@ def get_files(path):
 
     return file_list, len(file_list)
 
+def put_test(img, str_text):
+    font = cv.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (10, 500)
+    fontScale = 1
+    fontColor = (0, 0, 255)
+    thickness = 1
+    lineType = 2
+
+    cv.putText(img, str_text,
+                bottomLeftCornerOfText,
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
 
 if __name__ == '__main__':
 
@@ -23,12 +39,12 @@ if __name__ == '__main__':
     # reader = Reader(['en'], gpu=True)
 
     # Using custom model
-    reader = Reader(['en'], gpu=False,
+    reader = Reader(['en'], gpu=True,
                     model_storage_directory='model',
                     user_network_directory='user_network',
                     recog_network='custom')
 
-    files, count = get_files('demo_image')
+    files, count = get_files('demo_image')  #orig 'examples'
 
     for idx, file in enumerate(files):
         filename = os.path.basename(file)
@@ -41,4 +57,10 @@ if __name__ == '__main__':
         # result[2]: confidence
         for (bbox, string, confidence) in result:
             print("filename: '%s', confidence: %.4f, string: '%s'" % (filename, confidence, string))
-            # print('bbox: ', bbox)
+            print('bbox: ', bbox)
+            img = cv.imread(file)
+            cv.rectangle(img, (int(bbox[0][0]), int(bbox[0][1])), (int(bbox[2][0]), int(bbox[2][1])), (0, 0, 255), 3)
+            put_test(img, string)
+            cv.imshow(filename, img)
+            cv.waitKey(1000)
+            cv.destroyWindow(filename)
